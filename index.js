583e1919cb370a4e13f80e67e67ac0b8a01c8168
Daniel Batubenga <daniel.batubenga@gmail.com>
2024-08-09 12:50:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Servir arquivos estáticos da pasta "public"
+app.use(express.static('public'));
+
+// Rota para a página principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
